0f931c48db31c78d9623bce3e6bd78cb403d1ce6
lukasepple <git@lukasepple.de>
2015-10-18 00:13:49 +0200

    Our Introduction parser shall fail
    Since parseOnly gives back an Either.

--- src/Network/Ricochet/Version.hs
+++ src/Network/Ricochet/Version.hs
@@ -4,7 +4,6 @@ module Network.Ricochet.Version
   ( Versions ()
   , Version ()
   , ConnectionHandler ()
-  , parseIntroduction
   ) where
 
 import           Prelude                    hiding (lookup)
@@ -17,6 +16,7 @@ import           Data.Attoparsec.ByteString
 import           Data.ByteString            (ByteString ())
 import qualified Data.ByteString            as B
 import           Data.Map                   (Map (), lookup)
+import           Data.Maybe                 (fromJust)
 import           Data.Monoid                ((<>))
 import           GHC.Word                   (Word8 ())
 
@@ -31,20 +31,9 @@ type ConnectionHandler = Connection -> Ricochet ()
 type Versions = Map Version ConnectionHandler
 
 -- | Parses Introduction and Version Negotiation of the protocol
-parseIntroduction :: Map Version ConnectionHandler -> Parser (Maybe [ConnectionHandler])
-parseIntroduction supportedVersions = failingParser <|> return Nothing
-  where failingParser = do
-          string "IM"
-          nVersions <- anyWord8
-          foldJustAround . (fmap . flip lookup $ supportedVersions) <$>
-            count (fromIntegral nVersions) anyWord8
-
--- | Takes a list of [Maybe a] and returns a Maybe [a]. During the process all Nothings are skipped.
--- Nothing is returened if either the list is empty or it only contains Nothings.
--- TODO: evaluate wether there is a more generic function doing the job
-foldJustAround :: [Maybe a] -> Maybe [a]
-foldJustAround  = foldl folder Nothing
-  where folder Nothing Nothing   = Nothing
-        folder Nothing (Just x)  = Just [x]
-        folder (Just l) Nothing  = Just l
-        folder (Just l) (Just x) = Just (l <> [x])
+introductionParser :: Map Version ConnectionHandler -> Parser [ConnectionHandler]
+introductionParser supportedVersions = do
+  string "IM"
+  nVersions <- anyWord8
+  fmap (fromJust . (flip lookup $ supportedVersions)) <$>
+    count (fromIntegral nVersions) anyWord8
