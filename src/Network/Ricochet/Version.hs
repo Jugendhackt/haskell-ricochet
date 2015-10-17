3fc515ae6ad5286af7986743831f5fc651548db1
shak-mar <shakmar@openmailbox.org>
2015-10-17 21:43:09 +0200

    Change Socket to Connection in Version.hs

--- src/Network/Ricochet/Version.hs
+++ src/Network/Ricochet/Version.hs
@@ -7,6 +7,6 @@ import           Data.Map               (Map ())
 import           GHC.Word               (Word8 ())
 import           Network.Socket         (Socket ())
 
--- | Each version (Word8) has its own Handler that takes
--- a Serversocket and does Ricochet actions
-type Versions = Map Word8 (Socket -> Ricochet ())
+-- | Each version (Word8) has its own Handler that takes a Connection which has
+-- completed version negotiation and does Ricochet actions
+type Versions = Map Word8 (Connection -> Ricochet ())
