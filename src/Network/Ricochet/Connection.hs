a0a6c0919a2fa6c606522731438a43e6d4e291d9
shak-mar <shakmar@openmailbox.org>
2015-10-17 23:32:01 +0200

    Add createState and awaitConnection and fix connectTo

--- src/Network/Ricochet/Connection.hs
+++ src/Network/Ricochet/Connection.hs
@@ -1,19 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Network.Ricochet.Connection
-  (connectTo) where
+  (createState, awaitConnection, connectTo) where
 
 import Network.Ricochet.Monad
 import Network.Ricochet.Types
 
+import Data.Monoid ((<>))
 import Control.Lens
 import           Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as B
-import Network        (PortID(..))
+import Network        (PortID(..), listenOn, accept)
 import Network.Socks5 (socksConnectTo)
+
+createState :: PortID -> IO RicochetState
+createState port = do
+  sock <- listenOn port
+  return $ MkRicochetState sock [] [] (PortNumber 9050)
+
+awaitConnection :: Ricochet ()
+awaitConnection = do
+  sock <- use serverSocket
+  (handle, _, _) <- liftIO $ accept sock
+  let connection = MkConnection handle [MkChannel 0 $ MkChannelType "im.ricochet.control-channel"] False B.empty
+  connections %= (<> [connection])
 
 connectTo :: String -> PortID -> Ricochet Connection
 connectTo domain port = do
   torPort <- use socksPort
   handle <- liftIO $ socksConnectTo "localhost" torPort domain port
-  return $ MkConnection handle [MkChannel 0 $ MkChannelType "im.ricochet.control-channel"] True B.empty
+  let connection = MkConnection handle [MkChannel 0 $ MkChannelType "im.ricochet.control-channel"] True B.empty
+  connections %= (<> [connection])
+  return connection
