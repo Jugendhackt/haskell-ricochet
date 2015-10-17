cbb6e19f9cfe111edc8d136ad84c79c42686c196
lukasepple <git@lukasepple.de>
2015-10-17 17:00:08 +0200

    Add Version handling
    Supported versions are a Map from a Version (Word8)
    to a handler that takes the server socket and returns
    the Ricochet () action for instant messaging.

--- shell.nix
+++ shell.nix
@@ -4,15 +4,16 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { cabal-install, mkDerivation, base, bytestring, lens, mtl, network
-      , network-anonymous-tor, stdenv, transformers
+  f = { cabal-install, mkDerivation, base, bytestring, containers, lens, mtl
+      , network, network-anonymous-tor, stdenv, transformers
       }:
       mkDerivation {
         pname = "haskell-ricochet";
         version = "0.1.0.0";
         src = ./.;
         libraryHaskellDepends = [ cabal-install
-          base bytestring lens mtl network network-anonymous-tor transformers
+          base bytestring containers lens mtl network network-anonymous-tor
+          transformers
         ];
         description = "ricochet reimplementation in Haskell";
         license = stdenv.lib.licenses.gpl3;
---
+++ src/Network/Ricochet/Version.hs
@@ -0,0 +1,12 @@
+module Network.Ricochet.Version
+  (Versions()) where
+
+import           Network.Ricochet.Types
+
+import           Data.Map               (Map ())
+import           GHC.Word               (Word8 ())
+import           Network.Socket         (Socket ())
+
+-- | Each version (Word8) has its own Handler that takes
+-- a Serversocket and does Ricochet actions
+type Versions = Map Word8 (Socket -> Ricochet ())
