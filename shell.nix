758d7ccfbc5856edc59d9f1ce08ba9872a5e06e7
shak-mar <shakmar@openmailbox.org>
2015-10-17 13:40:00 +0200

    Add Lens dependency

--- shell.nix
+++ shell.nix
@@ -4,19 +4,19 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, mtl, network, stdenv }:
+  f = { cabal-install, mkDerivation, base, lens, mtl, network, stdenv }:
       mkDerivation {
         pname = "haskell-ricochet";
         version = "0.1.0.0";
         src = ./.;
-        libraryHaskellDepends = [ base mtl network ];
+        libraryHaskellDepends = [ cabal-install base lens mtl network ];
         description = "ricochet reimplementation in Haskell";
         license = stdenv.lib.licenses.gpl3;
       };
 
   haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
+                      then pkgs.haskellPackages
+                      else pkgs.haskell.packages.${compiler};
 
   drv = haskellPackages.callPackage f {};
 
