e7a20945b95f893a8b8752f617d3e3e72351aca5
shak-mar <shakmar@openmailbox.org>
2015-10-17 21:58:15 +0200

    Fix Version.hs

--- src/Network/Ricochet/Version.hs
+++ src/Network/Ricochet/Version.hs
@@ -2,10 +2,10 @@ module Network.Ricochet.Version
   (Versions()) where
 
 import           Network.Ricochet.Monad
+import           Network.Ricochet.Types
 
 import           Data.Map               (Map ())
 import           GHC.Word               (Word8 ())
-import           Network.Socket         (Socket ())
 
 -- | Each version (Word8) has its own Handler that takes a Connection which has
 -- completed version negotiation and does Ricochet actions
