dc7e85b6f995b1784eb31cb78dfaabbeff73bd43
lukasepple <git@lukasepple.de>
2015-10-17 10:39:26 +0200

    Initital commit
    adding boilerplate

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "haskell-ricochet";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base ];
+        description = "ricochet reimplementation in Haskell";
+        license = stdenv.lib.licenses.gpl3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
