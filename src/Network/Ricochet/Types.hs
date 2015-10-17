48bed73ef4c2998ef964d6d7d17ca3310dbf7535
shak-mar <shakmar@openmailbox.org>
2015-10-17 13:42:54 +0200

    Change Ricochet Monad to StateT and add some lenses

--- src/Network/Ricochet/Types.hs
+++ src/Network/Ricochet/Types.hs
@@ -3,16 +3,32 @@ module Network.Ricochet.Types
   (Ricochet(..)) where
 
 import           Control.Applicative    (Applicative (..))
+import           Control.Lens
 import           Control.Monad.IO.Class (MonadIO (..))
-import           Control.Monad.Reader   (MonadReader (..), ReaderT (..))
+import           Control.Monad.State    (MonadState (..), StateT (..))
 import           Network.Socket         (Socket ())
 
 
-newtype Ricochet a = Ricochet { runRicochet :: ReaderT Socket IO a }
+newtype Ricochet a = Ricochet { runRicochet :: StateT RicochetState IO a }
   deriving ( Functor, Applicative, Monad
-           , MonadIO, MonadReader Socket)
+           , MonadIO, MonadState RicochetState)
 
-data Packet = MkPacket {
-    size       :: Int
-  , channelID  :: Int
-  , packetData :: ByteString }
+data Packet = MkPacket
+  { _size       :: Int
+  , _channelID  :: Int
+  , _packetData :: ByteString }
+
+makeLenses ''Packet
+
+data RicochetState = MkRicochetState
+  { _serverSocket :: Socket
+  , _connections :: [Connection]
+  }
+
+makeLenses ''RicochetState
+
+data Connection = MkConnection
+  { _socket :: Socket
+  }
+
+makeLenses ''Connection
