4a514229296515c14dd734c503c86a04b74b08c1
shak-mar <shakmar@openmailbox.org>
2015-10-18 09:26:28 +0200

    Add version to server demo

--- src/Demo/Server.hs
+++ src/Demo/Server.hs
@@ -7,13 +7,16 @@ import           Network.Ricochet.Connection
 import           Network.Ricochet.Monad
 
 import           Control.Monad.State
+import           Control.Lens
 import           Data.ByteString             (ByteString (), pack)
-import           Data.Map                    (empty)
+import qualified Data.Map                    as M
 import           Network                     hiding (connectTo)
 
 main = do
   state <- createState (PortNumber 9878)
   flip (runStateT . runRicochet) state $ do
+    versions %= (M.insert 20 (\con -> do
+      liftIO $ putStrLn "Version 20"))
     con <- awaitConnection
     packet <- nextPacket con
     liftIO . print $ packet
