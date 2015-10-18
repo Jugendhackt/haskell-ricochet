eaf8150cb50337a4a9a5584e78dbeaa320fce155
lukasepple <git@lukasepple.de>
2015-10-18 11:23:59 +0200

    Update Server Demo

--- src/Demo/Server.hs
+++ src/Demo/Server.hs
@@ -5,11 +5,13 @@ module Main where
 import           Network.Ricochet
 import           Network.Ricochet.Connection
 import           Network.Ricochet.Monad
+import           Network.Ricochet.Types
 
-import           Control.Monad.State
 import           Control.Lens
+import           Control.Monad.State
 import           Data.ByteString             (ByteString (), pack)
 import qualified Data.Map                    as M
+import           Data.Monoid                 ((<>))
 import           Network                     hiding (connectTo)
 
 main = do
@@ -18,5 +20,8 @@ main = do
     versions %= (M.insert 20 (\con -> do
       liftIO $ putStrLn "Version 20"))
     con <- awaitConnection
-    packet <- nextPacket con
-    liftIO . print $ packet
+    sendByteString con 0x00 "Jeremy ist total cute!"
+    forever $ do
+      p <- nextPacket con
+      sendByteString con 0x00 $ "PONG " <> (p ^. pPacketData)
+      liftIO $ print p
