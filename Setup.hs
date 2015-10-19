2cd8383aee81e585157cef3c4a25f1c55b14bee8
shak-mar <shakmar@openmailbox.org>
2015-10-19 22:24:07 +0200

    Lay foundations for hprotoc as preConf cabal hook
    This lays the foundation for #10.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,15 @@
 import Distribution.Simple
-main = defaultMain
+import Data.Monoid((<>))
+
+uncurry2 :: (((a, b) -> c) -> ((a1, b1) -> c1) -> d) -> ((a -> b -> c) -> (a1 -> b1 -> c1) -> d)
+uncurry2 = flip (.) uncurry . flip . flip (.) uncurry . flip
+
+combine' :: (Monad m, Monoid b) => (a -> m b) -> (a -> m b) -> (a -> m b)
+combine' f g x = f x >>= \rf -> g x >>= \rg -> return $ rf <> rg
+
+combine :: (Monad m, Monoid c) => (a -> b -> m c) -> (a -> b -> m c) -> (a -> b -> m c)
+combine = fmap (fmap curry) $ uncurry2 combine'
+
+runHprotoc = return () -- TODO: Run hprotoc
+preConfHook = preConf simpleUserHooks `combine` const (const (runHprotoc >> return mempty))
+main = defaultMainWithHooks $ simpleUserHooks { preConf = preConfHook }
