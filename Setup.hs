8111f2f630e73f40ce0043c96d786120d01bf7a7
shak-mar <shakmar@openmailbox.org>
2015-10-20 13:25:50 +0200

    Call hprotoc at build time

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,19 @@
 import Distribution.Simple
-import Data.Monoid((<>))
+
+import Data.Monoid    ((<>))
+import System.Process (callProcess)
+
+runHprotoc = callProcess "hprotoc"
+  [ "--haskell_out=src/Network/Ricochet"
+  , "--proto_path=src/Network/Ricochet/Protocol/proto"
+  , "AuthHiddenService.proto"
+  , "ChatChannel.proto"
+  , "ContactRequestChannel.proto"
+  , "ControlChannel.proto"
+  ]
+
+preConfHook = preConf simpleUserHooks `combine` const (const (runHprotoc >> return mempty))
+main = defaultMainWithHooks $ simpleUserHooks { preConf = preConfHook }
 
 uncurry2 :: (((a, b) -> c) -> ((a1, b1) -> c1) -> d) -> ((a -> b -> c) -> (a1 -> b1 -> c1) -> d)
 uncurry2 = flip (.) uncurry . flip . flip (.) uncurry . flip
@@ -9,7 +23,3 @@ combine' f g x = f x >>= \rf -> g x >>= \rg -> return $ rf <> rg
 
 combine :: (Monad m, Monoid c) => (a -> b -> m c) -> (a -> b -> m c) -> (a -> b -> m c)
 combine = fmap (fmap curry) $ uncurry2 combine'
-
-runHprotoc = return () -- TODO: Run hprotoc
-preConfHook = preConf simpleUserHooks `combine` const (const (runHprotoc >> return mempty))
-main = defaultMainWithHooks $ simpleUserHooks { preConf = preConfHook }
--- shell.nix
+++ shell.nix
@@ -5,24 +5,29 @@ let
   inherit (nixpkgs) pkgs;
 
   f = { cabal-install, mkDerivation, attoparsec, base, bytestring, containers
-      , lens, mtl, network, network-anonymous-tor, socks, stdenv
+      , hprotoc, lens, mtl, network, network-anonymous-tor, socks, stdenv
       , transformers
       }:
       mkDerivation {
         pname = "haskell-ricochet";
         version = "0.1.0.0";
         src = ./.;
+        isLibrary = true;
+        isExecutable = true;
         libraryHaskellDepends = [ cabal-install
-          attoparsec base bytestring containers lens mtl network
+          attoparsec base bytestring containers hprotoc lens mtl network
           network-anonymous-tor socks transformers
+        ];
+        executableHaskellDepends = [
+          base bytestring containers lens mtl network
         ];
         description = "ricochet reimplementation in Haskell";
         license = stdenv.lib.licenses.gpl3;
       };
 
   haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
+                      then pkgs.haskellPackages
+                      else pkgs.haskell.packages.${compiler};
 
   drv = haskellPackages.callPackage f {};
 
