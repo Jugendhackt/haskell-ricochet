9b54a8a416c534d767066d2b0241668d1f908c28
shak-mar <shakmar@openmailbox.org>
2015-10-25 00:45:35 +0200

    Add documentation to the protobuf helper lenses

--- src/Network/Ricochet/Protocol/Protobuf.hs
+++ src/Network/Ricochet/Protocol/Protobuf.hs
@@ -1,3 +1,12 @@
+{-|
+  Module:      Network.Ricochet.Protocol.Protobuf
+  Description: Helper lenses for dealing with Protobuf messages
+
+"Network.Ricochet.Protocol.Protobuf" defines lenses that enable you to dump and
+parse Google Protobuf messages, and inspect their extension values, given the
+keys.
+-}
+
 {-# LANGUAGE RankNTypes #-}
 module Network.Ricochet.Protocol.Protobuf
   ( ext
@@ -9,8 +18,11 @@ import           Control.Monad
 import           Data.ByteString          (ByteString ())
 import           Text.ProtocolBuffers
 
+-- | Take an extension key and return a Traversal' that yields all the values to
+-- that extension key
 ext :: (MonadPlus c, ExtKey c) => Key c msg v -> Traversal' msg (c v)
 ext k = lens ((^? _Right) . getExt k) (flip $ putExt k . maybe mzero id) . _Just
 
+-- | A Prism' that can dump and parse Protobuf messages
 msg :: (ReflectDescriptor msg, Wire msg) => Prism' ByteString msg
 msg = lazy . prism' messagePut ((^? _Right . _1) . messageGet)
