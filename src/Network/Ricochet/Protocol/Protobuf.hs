7ce390def6a1d4e738dadd3cca95b10efb753fd5
shak-mar <shakmar@openmailbox.org>
2015-10-25 00:26:11 +0200

    Add Prism' to deal with Protobuf messages

--- src/Network/Ricochet/Protocol/Protobuf.hs
+++ src/Network/Ricochet/Protocol/Protobuf.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE RankNTypes #-}
 module Network.Ricochet.Protocol.Protobuf
   ( ext
+  , msg
   ) where
 
 import           Control.Lens
 import           Control.Monad
+import           Data.ByteString          (ByteString ())
 import           Text.ProtocolBuffers
 
 ext :: (MonadPlus c, ExtKey c) => Key c msg v -> Traversal' msg (c v)
 ext k = lens ((^? _Right) . getExt k) (flip $ putExt k . maybe mzero id) . _Just
+
+msg :: (ReflectDescriptor msg, Wire msg) => Prism' ByteString msg
+msg = lazy . prism' messagePut ((^? _Right . _1) . messageGet)
