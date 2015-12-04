8cb144f691b99f47bce8b34cc16bbba19fe8d14f
shak-mar <shakmar@openmailbox.org>
2015-12-04 18:30:44 +0100

    Add torDomain function to Crypto module

--- src/Network/Ricochet/Crypto.hs
+++ src/Network/Ricochet/Crypto.hs
@@ -12,24 +12,31 @@ elsewhere.
 module Network.Ricochet.Crypto
   ( generate1024BitRSA
   , hmacSHA256
+  , hashSHA1
   , sign
   , verify
   , publicDER
   , privateDER
   , base64
   , public
+  , torDomain
   )
 where
 
 import           Network.Ricochet.Crypto.RSA
 
-import           Control.Lens               (Prism', Review, (^?), _Right,
+import           Control.Lens               (Prism', Review, (^?), (#), _Right,
                                              prism', to, unto)
 import           Data.ByteString            (ByteString)
-import           Data.ByteString.Base64     (encode, decode)
+import qualified Data.ByteString            as B (take)
+import qualified Data.ByteString.Base32     as B32 (encode)
+import           Data.ByteString.Base64     as B64 (encode, decode)
+import qualified Data.ByteString.Char8      as B8 (map)
+import           Data.Char                  (toLower)
 import           Data.Maybe                 (fromJust)
 import           OpenSSL                    (withOpenSSL)
-import           OpenSSL.EVP.Digest         (Digest, getDigestByName, hmacBS)
+import           OpenSSL.EVP.Digest         (Digest, getDigestByName, hmacBS,
+                                             digestBS)
 import           OpenSSL.EVP.PKey           (KeyPair, PublicKey)
 import           OpenSSL.EVP.Sign           (signBS)
 import           OpenSSL.EVP.Verify         (VerifyStatus, verifyBS)
@@ -45,11 +52,19 @@ generate1024BitRSA = generateRSAKey' 1024 3
 sha256 :: Digest
 sha256 = fromJust . unsafePerformIO . withOpenSSL $ getDigestByName "SHA256"
 
+-- | The SHA1 Hash algorithm
+sha1 :: Digest
+sha1 = fromJust . unsafePerformIO . withOpenSSL $ getDigestByName "SHA1"
+
 -- | Perform a secret key signing using HMAC-SHA256
 hmacSHA256 :: ByteString -- ^ The (shared) secret key
            -> ByteString -- ^ The data to be signed
            -> ByteString -- ^ The resulting HMAC signature
 hmacSHA256 key bs = hmacBS sha256 key bs
+
+-- | Hashes a 'ByteString' using 'sha1'.
+hashSHA1 :: ByteString -> ByteString
+hashSHA1 = digestBS sha1
 
 -- | Sign a ByteString using the algorithm corresponding to the passed KeyPair
 --   and SHA256.
@@ -76,8 +91,12 @@ privateDER = prism' toDERPriv fromDERPriv
 
 -- | A Prism that allows Base64 encoding and decoding of ByteStrings
 base64 :: Prism' ByteString ByteString
-base64 = prism' encode (^? to decode . _Right)
+base64 = prism' B64.encode (^? to B64.decode . _Right)
 
 -- | This allows you to use an RSAKey where a RSAPubKey is required.
 public :: RSAKey k => Review RSAPubKey k
 public = unto $ unsafePerformIO . rsaCopyPublic
+
+-- | Compute the Tor domain name of some RSA key
+torDomain :: RSAKey k => k -> ByteString
+torDomain k = B8.map toLower . B32.encode . B.take 10 . hashSHA1 $ publicDER . public # k
