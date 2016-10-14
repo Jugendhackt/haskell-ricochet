aa84fb96b2e73c6ea18ecb0c95d6b5c9b9e9bacb
blueburningcoder <f.karg10@gmail.com>
2016-10-14 16:47:03 +0200

    tupleToAddress workaround, explicit depency
    - dependency: haskell-src-exts == 1.17.1 (<1.18), because hprotoc
      doesn't build with higher than that version
    
    - toupleToAddress: for some reason cabal as well as stack said that
      tupleToAddress would not be exported by Network.Socket, so the
      workaround is included

--- src/Network/Ricochet/Monad.hs
+++ src/Network/Ricochet/Monad.hs
@@ -1,15 +1,17 @@
-{-|
-  Module:      Network.Ricochet.Monad
-  Description: Implementation of the 'Ricochet'-Monad
 
-"Network.Ricochet.Monad" contains the definition and implementation of the
-'Ricochet'-Monad, as well as some useful functions in it.
+{- |
+     Module:      Network.Ricochet.Monad
+     Description: Implementation of the 'Ricochet'-Monad
+
+     "Network.Ricochet.Monad" contains the definition and implementation of the
+     'Ricochet'-Monad, as well as some useful functions in it.
 -}
 
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE NoMonomorphismRestriction  #-}
+
 module Network.Ricochet.Monad
   ( Ricochet (..)
   , RicochetState (..)
@@ -41,7 +43,7 @@ import Data.Text (toLower)
 import Data.Text.Encoding (encodeUtf8)
 import Data.Word (Word8, Word16)
 import Network.Socket (Socket (), PortNumber (..), socket, bind,
-                       tupleToHostAddress, SocketType (..), defaultProtocol,
+                       inet_addr, SocketType (..), defaultProtocol,
                        SockAddr (..), Family (..))
 import Network.BSD (getServicePortNumber)
 import Network.Anonymous.Tor (mapOnion, withSession)
@@ -63,10 +65,14 @@ data RicochetState = MkRicochetState
   , _versions     :: Map Word8 (Connection -> Ricochet ()) -- ^ A map mapping version numbers to handlers
   }
 
+-- | Lenses for RicochetState
+--
 makeLenses ''RicochetState
+
 
 -- | Checks if a complete packet is available on the given connection, and if
 --   so, reads and returns it.
+--
 peekPacket :: Connection -> Ricochet (Maybe Packet)
 peekPacket con = do
   readBytes <- liftIO $ B.hGetNonBlocking (con ^. cHandle) maxPacketSize
@@ -81,31 +87,40 @@ peekPacket con = do
       return $ Just packet
     Unfinished -> return Nothing
     Failure    -> return Nothing
+    -- Todo: quit connection for unreadable packages?
   where maxPacketSize = fromIntegral (maxBound :: Word16)
         con'          = connections . look con
         chan' p       = con' . cChannels . lookWith cChannelID (p ^. pChannelID)
 
+
 -- | Waits for a complete packet to arrive and returns it
+--
 nextPacket :: Connection -> Ricochet Packet
 nextPacket con = do
   maybePacket <- peekPacket con
   case maybePacket of
     Just pkt -> return pkt
     Nothing -> liftIO (threadDelay delay) >> nextPacket con
-  where delay = round $ 10 ** 6
+  where delay = 10 ^ 6 -- μs -> 1s
+
 
 -- | Sends a Packet to a connected User
+--
 sendPacket :: Connection -> Packet -> Ricochet ()
 sendPacket con pkt = liftIO . B.hPutStr (con ^. cHandle) $ dumpPacket pkt
 
+
 -- | Packs a ByteString into Packets and sends it through the given Connection
+--
 sendByteString :: Connection -- ^ The Connection through which to send the ByteString
                -> Word16     -- ^ The ID of the channel the ByteString should be sent on
                -> ByteString -- ^ The ByteString to be sent
                -> Ricochet ()
 sendByteString con chan bs = mapM_ (sendPacket con) $ splitIntoPackets chan bs
 
+
 -- | Closes a connection and removes it from the list of connections
+--
 closeConnection :: Connection -> Ricochet ()
 closeConnection connection = do
   connections %= delete connection
@@ -120,10 +135,12 @@ data RicochetConfig = RicochetConfig
                                                           --   corresponding 'Connection' handlers
   }
 
+
 -- | Start an action inside the Ricochet monad.
 --
 --   This function uses a Tor control socket to create the hidden service.
 --   If no key is supplied, the hidden service will be random
+--
 startRicochet :: RicochetConfig
               -> [Contact]        -- ^ A list of known 'Contact's
               -> Ricochet ()
@@ -136,10 +153,12 @@ startRicochet config contacts action = do
       cPort     = rcSocksPort config
 
   listenSock <- socket AF_INET Stream defaultProtocol
-  bind listenSock (SockAddrInet listenInt (tupleToHostAddress (127,0,0,1)))
+  addr <- inet_addr "127.0.0.1"
+  bind listenSock (SockAddrInet listenInt addr)
   void . withSession ctrlInt $ \ctrlSock -> do
       address <- encodeUtf8 . toLower . toText <$> mapOnion ctrlSock listenInt listenInt False key
       startRicochet' listenSock address contacts (rcSocksPort config) (rcHandlers config) action
+
 
 -- | Start an action inside the Ricochet monad.
 --
@@ -147,6 +166,7 @@ startRicochet config contacts action = do
 --   hidden service via the torrc or some other method.
 --
 --   DON'T use this function if that's not the case.
+--
 startRicochet' :: Socket      -- ^ Socket to listen on
               -> ByteString   -- ^ Domain of the already configured hidden service
               -> [Contact]    -- ^ A list of known contacts
